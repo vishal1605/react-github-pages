6060fbc42672ca532b2f3e3ceabd1e501759cc62
vishal_gupta <vishal.gupta@myashida.com>
2024-05-10 11:51:51 +0530

    test for deploy on github pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    strictPort: true,
-    port: 4000
-  }
+  // server: {
+  //   host: true,
+  //   strictPort: true,
+  //   port: 4000
+  // }
+  base: '/test/'
 })
